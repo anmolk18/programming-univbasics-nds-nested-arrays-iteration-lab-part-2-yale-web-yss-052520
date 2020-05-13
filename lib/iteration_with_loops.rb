7220f58b3785fca474d31720c1bbdb5ca6879808
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-13 17:29:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,6 +4,8 @@ def find_min_in_nested_arrays(src)
 outer_results = []
 row_index = 0 
   while row_index < src.count do
-    element_
-    index 
+    element_index = 0
+    min_num =  
+    while element_index < src[row_index].count do 
+      if src[row_index][element_index] 
 end
