2be51e2041b6910ff6aa93823193831a7a4d44e8
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-13 17:36:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,14 @@ outer_results = []
 row_index = 0 
   while row_index < src.count do
     element_index = 0
-    min_num =  
+    min_num = Float::INFINITY 
     while element_index < src[row_index].count do 
-      if src[row_index][element_index] 
+      if src[row_index][element_index] < min_num 
+        min_num = src[row_index][element_index]
+      end 
+      element_index+= 1 
+    end
+    outer_results << min_num
+    row_index += 1 
 end
+outer_results 
